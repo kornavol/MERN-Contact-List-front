8da0aad0d43f8671b87e855c9b2aea6a912591f0
kornavol <volosh.andrey@gmail.com>
2021-05-26 16:02:51 +0200

    auth

---
+++ src/pages/Auth.js
@@ -0,0 +1,70 @@
+import './Auth.css';
+import { useHistory } from 'react-router-dom';
+
+
+
+export default function (props) {
+
+    let history = useHistory();
+
+    let submitHandler = (e) => {
+        e.preventDefault();
+
+        let data = {};
+
+        data.email = e.target[0].value;
+        data.pass = e.target[1].value;
+
+        let urlRegister = 'http://localhost:8080/auth/register';
+        let urlLogin = 'http://localhost:8080/auth/login';
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify(data)
+        }
+
+        if (e.nativeEvent.submitter.id == "register") {
+            fetch(urlRegister, options).then(result => result.json().then(output => {
+                if (output.status == 'success') {
+                    alert('Congrats, you registered as well! Please login.')
+                } else {
+                    alert(output.message)
+                }
+                // console.log(output);
+            }));
+        } else if (e.nativeEvent.submitter.id == "login") {
+            fetch(urlLogin, options)
+                .then(result => result.json()
+                    .then(output => {
+                        console.log(output);
+                        if (output.status == 'success') {
+                            alert(output.status)
+                            localStorage.setItem('token', output.token);
+
+                            history.push("/contacts");
+
+                            // props.setIsLoggedIn(true);
+                        } else {
+                            alert(output.status)
+                            // localStorage.removeItem('token');
+                        }
+                    }));
+        }
+    }
+
+    return (
+        <div className="wrapper">
+            <form onSubmit={submitHandler}>
+                <input name="email" type="email" />
+                <input name="pass" type="password" />
+                <section>
+                    <input id="register" type="submit" value="Register" />
+                    <input id="login" type="submit" value="Login" />
+                </section>
+            </form>
+        </div>
+    )
+}
---
+++ src/pages/Contact.js
@@ -0,0 +1,104 @@
+import { useState, useEffect } from "react";
+
+
+import Card from "../componets/Card"
+
+function Contacts() {
+
+    const [form, setForm] = useState({ fullName: '', email: '', phone: '', address: '' });
+    const [contacts, setContacts] = useState([{
+        _id: '1',
+        fullName: 'test',
+        email: 't@t.com',
+        phone: '123456',
+        address: 'test'
+    }
+    ]);
+
+    const fillForm = (e, field) => {
+        let newForm = { ...form };
+        newForm[field] = e.target.value;
+        setForm(newForm);
+    }
+
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+        const url = 'http://localhost:8080/contacts/new';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-auth-tocken': localStorage.getItem('token')
+            },
+            // mode: "no-cors",  /*  crached my app. Why?? Maybe becouse od post method */
+            body: JSON.stringify(form)
+        }
+
+        fetch(url, options)
+            .then(data => data.json().then(output => setContacts([...contacts, output])));
+    }
+
+    useEffect(() => {
+        const url = 'http://localhost:8080/contacts/all';
+        const options = {
+            mode: 'cors',
+            headers: {
+                
+                'x-auth-tocken': localStorage.getItem('token')
+            }
+        }
+
+        fetch(url, options).then(data => data.json().then(contacts => {
+            setContacts(contacts);
+        }));
+    }, []);
+
+    const deleteContactHandler = (id) => {
+        const url = 'http://localhost:8080/contacts/delete/' + id;
+        const options = {
+            method: 'DELETE'
+        }
+
+        fetch(url, options)
+            .then(response => response.json().then(output => {
+                if (output.status === 'success') {
+                    alert(output.message);
+                    let newList = contacts.filter(contact => {
+                        if (contact._id != output.data) {
+                            return contact;
+                        }
+                    });
+                    setContacts(newList);
+                } else {
+                    alert(`There's an error. For details please check the console.`);
+                    console.log(output.message);
+                }
+            }))
+            .catch(err => { alert(err) });
+    }
+
+    const cards = contacts.map(contact => <Card
+        key={contact['_id']}
+        contact={contact}
+        deleteContact={deleteContactHandler.bind(this, contact['_id'])}
+    />);
+
+    /* console.log(cards, contacts) */
+    return (
+        <div className="App">
+            <form className="form" onSubmit={formSubmitHandler}>
+                <input required placeholder="Full name" value={form.fullName} onChange={(e) => fillForm(e, 'fullName')} />
+                <input type="email" placeholder="Email" value={form.email} onChange={(e) => fillForm(e, 'email')} />
+                <input type="tel" placeholder="Phone number" value={form.phone} onChange={(e) => fillForm(e, 'phone')} />
+                <input placeholder="Address" value={form.address} onChange={(e) => fillForm(e, 'address')} />
+                <button>Create Contact</button>
+            </form>
+
+            <section className="contact-list">
+                {cards}
+            </section>
+        </div>
+    );
+}
+
+export default Contacts;
