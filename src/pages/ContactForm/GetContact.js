1f822b7252e39a9a99b4c5c437096e2e0a1fbba9
kornavol <volosh.andrey@gmail.com>
2021-06-08 15:17:50 +0200

    upload multuple files

--- src/pages/ContactForm/GetContact.js
+++ src/pages/ContactForm/GetContact.js
@@ -1,59 +1,65 @@
-import './ContactForm.css';
+import "./ContactForm.css";
 
 const headers = {
-    'Content-Type': 'application/json',
+    // "Content-Type": "application/json",
     // 'x-auth-tocken': localStorage.getItem('token')
-}
-
-
+};
 
 const GetContact = () => {
-
     function SubmitHandler(e) {
         e.preventDefault();
 
-        let data = {};
+        let data = new FormData();
 
-        data.fullName = e.target[0].value
-        data.email = e.target[1].value
-        data.phone = e.target[2].value
-        data.message = e.target[3].value
+        const attachs = e.target[4].files;
 
+        data.append("fullName", e.target[0].value);
+        data.append("email", e.target[1].value);
+        data.append("phone", e.target[2].value);
+        data.append("message", e.target[3].value);
 
+        console.log(typeof attachs);
 
-        const url = 'http://localhost:8080/get-contact'; //?
-        const options = {
-            method: 'POST',
-            headers,
-            // mode: "no-cors",  /*  crached my app. Why?? Maybe becouse od post method */
-            body: JSON.stringify(data)
+        /* Because we coudnt send all files into array. We need to appeand each file separetly 
+            with  THE SAME KEY.
+            For this we have to use "forin" loop becouse typeof varible is object (forEach working only with array) 
+        */
+        if (attachs.length != 0) {
+            for (const single_file of attachs) {
+                data.append("attachs", single_file);
+            }
         }
 
-        fetch(url, options)
-            .then(data => data.json().then(output => {
-                if (output.status === 'success') {
-                    console.log('Congats');
+        const url = "http://localhost:8080/get-contact"; //?
+        const options = {
+            method: "POST",
+            body: data,
+        };
+
+        fetch(url, options).then((data) =>
+            data.json().then((output) => {
+                if (output.status === "success") {
+                    console.log("Congats");
                 } else {
                     console.log(output.message);
                 }
-
-            }));
+            })
+        );
     }
-
-
 
     return (
         <div id="get-contact">
             <h1>Get Contact</h1>
             <form className="contact-form" onSubmit={SubmitHandler}>
-                <input type='text' placeholder="Full Name" />
-                <input type='email' placeholder="Email" />
-                <input type='text' placeholder="Phone" />
-                <textarea type='text' placeholder="Message" />
+                <input type="text" placeholder="Full Name" />
+                <input type="email" placeholder="Email" />
+                <input type="text" placeholder="Phone" />
+                <textarea type="text" placeholder="Message" />
+                <input type="file" multiple />
                 <button>Get Contact</button>
             </form>
         </div>
     );
-}
+};
 
 export default GetContact;
